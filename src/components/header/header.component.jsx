a89a2f4a2a248f91e4afa34ce3a7e22eb093beac
SergiuAlexandruPop <sergiualexandrupop@gmail.com>
2020-04-22 20:22:19 +0300

    integrated firebase into aplication, created sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -18,6 +20,15 @@ const Header = () => {
         <Link className="option" to="/shop">
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            Sign Out
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyCMbBHCJMWioue2lmVbUGr75gTtw0rRT-A",
+  authDomain: "crwn-db-812ff.firebaseapp.com",
+  databaseURL: "https://crwn-db-812ff.firebaseio.com",
+  projectId: "crwn-db-812ff",
+  storageBucket: "crwn-db-812ff.appspot.com",
+  messagingSenderId: "803473684292",
+  appId: "1:803473684292:web:1fb902f919d977f0fe4911",
+  measurementId: "G-LMN0520449",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
